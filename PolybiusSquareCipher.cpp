9b60d0543fc20a98235213d15c3b4d4fb2daf155
Diellza <raqidiellza@gmail.com>
2023-04-15 00:11:13 +0200

    Funksioni polybiusCipher

--- PolybiusSquareCipher.cpp
+++ PolybiusSquareCipher.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+void polybiusCipher(string s) {
+    int rreshti, kolona;
+
+    for (int i = 0; s[i]; i++) {
+
+        if (isupper(s[i])) {
+
+            rreshti = ceil((s[i] - 'A') / 5) + 1;
+            kolona = ((s[i] - 'A') % 5) + 1;
+        }
+    }
+}
 
 int main(){
 
