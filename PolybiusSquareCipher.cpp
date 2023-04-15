d397bfb0ccff54a9e3bf88ec2519e2134e121f4d
MalsorAbdyli <malsor.abdyli@student.uni-pr.edu>
2023-04-15 17:50:50 +0200

    Update PolybiusSquareCipher.cpp

--- PolybiusSquareCipher.cpp
+++ PolybiusSquareCipher.cpp
@@ -3,11 +3,12 @@
 
 using namespace std;
 
-
+//numri i rreshtave
 const int RRESHTAT = 5;
+//numri i kolonave
 const int KOLONAT = 5;
 
-
+//tabela POLYBIUS SQUARE
 char tabela[RRESHTAT][KOLONAT] = {
     {'a', 'b', 'c', 'd', 'e'},
     {'f', 'g', 'h', 'i', 'k'},
@@ -18,42 +19,52 @@ char tabela[RRESHTAT][KOLONAT] = {
 
 void polybiusSquare() {
 
+    //headeri
     cout << "  1 2 3 4 5" << endl;
 
+    //numri i rreshtave
     for (int i = 0; i < RRESHTAT; i++) {
         cout << i + 1 << " ";
 
+         //shfaqja e karakterit perkatës për çdo kolonë të rreshtit
         for (int j = 0; j < KOLONAT; j++) {
             cout << tabela[i][j] << " ";
         }
         cout << endl;
     }
 }
-
-
-
+// funksioni për shfaqjen e tekstit si POLYBIUS SQUARE CIPHER
 void polybiusCipher(string s) {
     int rreshti, kolona;
 
+    // konvertimi i çdo karakteri në kod të enkriptuar
     for (int i = 0; s[i]; i++) {
-
+        
+        // nëse karakteri është me shkronjë të madhe
         if (isupper(s[i])) {
-
+            // gjetja e rreshtit të tabelës
             rreshti = ceil((s[i] - 'A') / 5) + 1;
+
+            // gjetja e kolonës së tabelës
             kolona = ((s[i] - 'A') % 5) + 1;
         }
 
-        else {
-
+        // nëse karakteri është me shkronjë të vogël
+        else {  
+            // gjetja e rreshtit të tabelës
             rreshti = ceil((s[i] - 'a') / 5) + 1;
+
+            // gjetja e kolonës së tabelës
             kolona = ((s[i] - 'a') % 5) + 1;
         }
 
+        // nëse karakteri është 'k'
         if (tolower(s[i]) == 'k') {
             rreshti = rreshti - 1;
             kolona = 5 - kolona + 1;
         }
 
+        // nëse karakteri është më i madh se 'j'
         else if (tolower(s[i]) >= 'j') {
             if (kolona == 1) {
                 kolona = 6;
@@ -68,7 +79,7 @@ void polybiusCipher(string s) {
 }
         
     
-
+//Main Kodi
 int main(){
 
     string s;
